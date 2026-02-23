83cebac70c5e73c8bed61d7aab4d9742114e6363
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-02-23 23:13:50 +0530

    Updated the prompt

--- query_engine.py
+++ query_engine.py
@@ -62,7 +62,7 @@ def ask(question: str, k: int = 4, debug: bool = False) -> str:
     )
 
     prompt ="""
-    Hey LLM, you are a senior data scientist.
+    Hey LLM, you are a senior data scientist. You have to analyase the jupyter notebook well
     """
 
 
