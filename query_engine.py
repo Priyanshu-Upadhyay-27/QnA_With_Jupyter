7f38fc9d95802d053ae049965a2e0121c41c47cc
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-02-23 23:14:55 +0530

    Make some spell correction

--- query_engine.py
+++ query_engine.py
@@ -62,7 +62,8 @@ def ask(question: str, k: int = 4, debug: bool = False) -> str:
     )
 
     prompt ="""
-    Hey LLM, you are a senior data scientist. You have to analyase the jupyter notebook well
+    Hey LLM, you are a senior data scientist. 
+    You have to analyse jupyter notebooks well and solve user query
     """
 
 
